e1317574708be144a16eb0834b47fa9849d2d613
Spordb <otomatik@spordb.local>
2026-06-12 06:09:56 +0300

    Mackolik Skor Güncelleme | 12.06.2026 06:09:55

--- maçkoliktamamlama.py
+++ maçkoliktamamlama.py
@@ -20,8 +20,8 @@ from selenium.common.exceptions import *
 # =========================
 # ⚙️ AYARLAR
 # =========================
-BASLANGIC_TARIHI = "23.05.2026"   # GG.AA.YYYY
-BITIS_TARIHI = "25.05.2026"       # GG.AA.YYYY
+BASLANGIC_TARIHI = "01.06.2026"   # GG.AA.YYYY
+BITIS_TARIHI = "12.05.2026"       # GG.AA.YYYY
 
 BASE_DIR = Path(__file__).resolve().parent
 MAC_JSON_PATH = BASE_DIR / "public" / "data" / "mac.json"
